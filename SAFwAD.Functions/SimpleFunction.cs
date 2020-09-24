d32683715be299162b7ed5ee2a66a1af32eb40ca
Rick van den Bosch <rickvdbosch@outlook.com>
2020-09-24 22:18:56 +0200

    Fixed namespace

--- SAFwAD.Functions/SimpleFunction.cs
+++ SAFwAD.Functions/SimpleFunction.cs
@@ -5,7 +5,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
-namespace SAFwAD
+namespace SAFwAD.Functions
 {
     public static class SimpleFunction
     {
